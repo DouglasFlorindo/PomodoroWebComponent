6a02e6c84cc30838da38c0f81a8573c75b57ff15
Douglas Florindo <douglasflorindodeassis@gmail.com>
2024-11-21 12:15:52 -0300

    dialog settings fully working

--- PomodoroWebComponent/Script/pomodoro.js
+++ PomodoroWebComponent/Script/pomodoro.js
@@ -7,6 +7,8 @@ class PomodoroClock extends HTMLElement {
         super();
         this.attachShadow({ mode: "open" });
 
+        this.elements = {};
+
         //Loads external fonts
         const fonts = document.createElement("style");
         fonts.textContent = "/* Code injected by <pomodoro-clock> */ @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap'); @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');";
@@ -17,7 +19,7 @@ class PomodoroClock extends HTMLElement {
         icons.setAttribute("rel", "stylesheet");
         icons.setAttribute("href", "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=pause,play_arrow,replay,settings,skip_next");
         document.head.appendChild(icons);
-        
+
         //Loads external css
         fetch('/PomodoroWebComponent/Style/pomodoroStyle.css')
             .then(response => response.text())
@@ -75,27 +77,44 @@ class PomodoroClock extends HTMLElement {
 
     //#region WebComponentMethods
 
+    getElement(selector) {
+        return this.shadowRoot.querySelector(selector);
+    };
+
     initWebElements() {
         try {
-            this.timeElement = this.shadowRoot.querySelector("#pmdr-time");
-            this.sessionTypeElement = this.shadowRoot.querySelector("#pmdr-session-type");
-            this.cycleElement = this.shadowRoot.querySelector("#pmdr-cycle");
-            this.progressBarElement = this.shadowRoot.querySelector("#pmdr-progress-bar");
-            this.buttonStartPauseElement = this.shadowRoot.querySelector("#pmdr-button-start-pause");
-            this.buttonStartPauseIconElement = this.shadowRoot.querySelector("#pmdr-button-start-pause-icon");
-            this.buttonRestartElement = this.shadowRoot.querySelector("#pmdr-button-restart");
-            this.buttonSkipElement = this.shadowRoot.querySelector("#pmdr-button-skip");
-            this.buttonSettingsElement = this.shadowRoot.querySelector("#pmdr-button-settings");
-            this.dialogSettingsElement = this.shadowRoot.querySelector("#pmdr-dialog-settings");
+            this.elements = {
+                textTimer: this.getElement("#pmdr-time"),
+                textSessionType: this.getElement("#pmdr-session-type"),
+                textCycle: this.getElement("#pmdr-cycle"),
+                SVGCircleProgressBar: this.getElement("#pmdr-progress-bar"),
+                buttonStartPause: this.getElement("#pmdr-button-start-pause"),
+                buttonStartPauseIcon: this.getElement("#pmdr-button-start-pause-icon"),
+                buttonRestart: this.getElement("#pmdr-button-restart"),
+                buttonSkip: this.getElement("#pmdr-button-skip"),
+                buttonSettings: this.getElement("#pmdr-button-settings"),
+                dialogSettings: this.getElement("#pmdr-dialog-settings"),
+                formSettings: this.getElement("#pmdr-form-settings"),
+                inputNumCycles: this.getElement('[setting="numCycles"]'),
+                inputPomodoroLengthMin: this.getElement('[setting="pomodoroLengthMin"]'),
+                inputShortBreakLengthMin: this.getElement('[setting="shortBreakLengthMin"]'),
+                inputLongBreakLengthMin: this.getElement('[setting="longBreakLengthMin"]'),
+                inputAutoStart: this.getElement('[setting="autoStart"]'),
+                buttonResetSettings: this.getElement("#pmdr-button-reset-settings")
+            };
+
 
             // this.dialogSettingsElement.showModal()
 
-            this.buttonStartPauseElement.addEventListener("click", () => {
-                this.buttonStartPauseElement.getAttribute("mode") == "start" ? this.startClock() : this.pauseClock();
+            //Events
+            this.elements.buttonStartPause.addEventListener("click", () => {
+                this.elements.buttonStartPause.getAttribute("mode") == "start" ? this.startClock() : this.pauseClock();
             });
-            this.buttonRestartElement.addEventListener("click", this.restartClock.bind(this));
-            this.buttonSkipElement.addEventListener("click", this.endSessionHandler.bind(this));
-            this.buttonSettingsElement.addEventListener("click", () => this.dialogSettingsElement.showModal())
+            this.elements.buttonRestart.addEventListener("click", this.restartClock.bind(this));
+            this.elements.buttonSkip.addEventListener("click", this.endSessionHandler.bind(this));
+            this.elements.buttonSettings.addEventListener("click", () => this.elements.dialogSettings.showModal());
+            this.elements.formSettings.addEventListener("submit", this.applyFormSettings.bind(this));
+            this.elements.buttonResetSettings.addEventListener("click", this.resetSettingsFormToDefault.bind(this));
 
         } catch (error) {
             console.error(error)
@@ -108,30 +127,21 @@ class PomodoroClock extends HTMLElement {
             // Updates timer
             const minutes = Math.floor(this.remainingSessionTimeMs / 60000);
             const seconds = Math.floor((this.remainingSessionTimeMs % 60000) / 1000);
-            this.timeElement.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            this.elements.textTimer.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
             // Updates progress bar
-            const radius = this.progressBarElement.getAttribute("r");
+            const radius = this.elements.SVGCircleProgressBar.getAttribute("r");
             const circunference = 2 * Math.PI * radius;
             const percentage = this.remainingTotalTimeMs / this.totalTimeMs;
             const offset = circunference - (percentage * circunference);
 
-            this.progressBarElement.setAttribute("stroke-dashoffset", offset);
+            this.elements.SVGCircleProgressBar.setAttribute("stroke-dashoffset", offset);
 
             // Updates cycle counter
-            this.cycleElement.textContent = `${String(this.currentCycleNum)}/${String(this.configs.numCycles)}`;
+            this.elements.textCycle.textContent = `${String(this.currentCycleNum)}/${String(this.configs.numCycles)}`;
 
             // Updates session type
-            this.sessionTypeElement.textContent = `${this.currentSessionType}`
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-
-    updateCycleElement() {
-        try {
-            
+            this.elements.textSessionType.textContent = `${this.currentSessionType}`
         } catch (error) {
             console.error(error)
         }
@@ -141,12 +151,52 @@ class PomodoroClock extends HTMLElement {
     updateStartPauseButton() {
         try {
             //Invert start/pause
-            this.buttonStartPauseElement.setAttribute("mode", this.intervalId === null ? "start" : "pause");
-            this.buttonStartPauseElement.getAttribute("mode") === "start" ? this.buttonStartPauseIconElement.textContent = "play_arrow" : this.buttonStartPauseIconElement.textContent = "pause"
+            this.elements.buttonStartPause.setAttribute("mode", this.intervalId === null ? "start" : "pause");
+            this.elements.buttonStartPause.getAttribute("mode") === "start" ? this.elements.buttonStartPauseIcon.textContent = "play_arrow" : this.elements.buttonStartPauseIcon.textContent = "pause"
 
         } catch (error) {
             console.error(error);
         }
+    }
+
+
+    updateSettingsForm() {
+
+        try {
+
+        } catch (error) {
+            console.error(`Error updating form settings: ${error}`);
+        }
+    }
+
+
+    resetSettingsFormToDefault() {
+        try {
+            this.elements.inputNumCycles.value = 4;
+            this.elements.inputPomodoroLengthMin.value = 25;
+            this.elements.inputShortBreakLengthMin.value = 5;
+            this.elements.inputLongBreakLengthMin.value = 15;
+            this.elements.inputAutoStart.checked = false;
+        } catch (error) {
+            console.error(`Error reseting settings: ${error}`);
+        }
+    }
+
+
+    applyFormSettings() {
+        try {
+            this.configs.numCycles = this.elements.inputNumCycles.value || 4;
+            this.configs.pomodoroLengthMin = this.elements.inputPomodoroLengthMin.value || 25;
+            this.configs.shortBreakLengthMin = this.elements.inputShortBreakLengthMin.value || 5;
+            this.configs.longBreakLengthMin = this.elements.inputLongBreakLengthMin.value || 15;
+            this.configs.autoStart = this.elements.inputAutoStart.checked || false;
+
+
+        } catch (error) {
+            console.error(`Error applying settings: ${error}`);
+        }
+
+        this.restartClock();
     }
 
     //#endregion
@@ -233,14 +283,12 @@ class PomodoroClock extends HTMLElement {
                 break;
         }
 
-        this.updateCycleElement();
-
         console.log(`Loading next session: 
             \nSession type: ${this.currentSessionType}
             \nSession time: ${this.remainingSessionTimeMs / 60 / 1000}
             \nRemaining total time: ${this.remainingTotalTimeMs / 60 / 1000}
             \nCurrent cycle: ${this.currentCycleNum}`);
-        
+
         //Doesn't auto start in the first session.
         if (this.currentCycleNum === 1 && this.currentSessionType === this.sessionTypes.POMODORO) return;
         if (this.configs.autoStart) this.startClock();
